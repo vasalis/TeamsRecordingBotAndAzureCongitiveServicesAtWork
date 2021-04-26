7783a392773e34ea55e7e10e81c4e2344e797fd8
Vassilis Salis <vsalis@outlook.com>
2021-04-26 15:09:52 +0300

    middleware iac

--- 03_IaC/00_AzureCLI/01_MiddleWare_iac.ps1
+++ 03_IaC/00_AzureCLI/01_MiddleWare_iac.ps1
@@ -38,12 +38,9 @@ Write-Output "About to create Text Analytics and Translation Cognitive Services:
 az cognitiveservices account create --name $textAnalyticsName --resource-group $resourceGroupName --kind TextAnalytics --sku S --location $azureLocation --yes
 az cognitiveservices account create --name $speechToTextName --resource-group $resourceGroupName --kind SpeechServices --sku S0 --location $azureLocation --yes
 
-# Sleep for a minute.
-Start-Sleep -Seconds 60
-
 # Get keys and urls as variables
 $textAnalyticsKey = az cognitiveservices account keys list --name $textAnalyticsName --resource-group $resourceGroupName --query 'key1'
-$textAnalyticsEndPoint = az cognitiveservices account show -n $textAnalyticsName -g $resourceGroupName --query endpoint --output tsv
+$textAnalyticsEndPoint = az cognitiveservices account show -n $textAnalyticsName -g $resourceGroupName --query 'endpoint'
 $speechToTextKey = az cognitiveservices account keys list --name $speechToTextName --resource-group $resourceGroupName --query 'key1'
 
 Write-Output "Textnalytics key is: $textAnalyticsKey, endpoint: $textAnalyticsEndPoint"
