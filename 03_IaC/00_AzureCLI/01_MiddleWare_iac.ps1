99202d9327a95f2441e2327505ec12666dc5d829
Vassilis Salis <vsalis@outlook.com>
2021-04-26 15:35:48 +0300

    middleware iac

--- 03_IaC/00_AzureCLI/01_MiddleWare_iac.ps1
+++ 03_IaC/00_AzureCLI/01_MiddleWare_iac.ps1
@@ -40,8 +40,7 @@ az cognitiveservices account create --name $speechToTextName --resource-group $r
 
 # Get keys and urls as variables
 $textAnalyticsKey = az cognitiveservices account keys list --name $textAnalyticsName --resource-group $resourceGroupName --query "key1"
-Write-Output az cognitiveservices account show -n $textAnalyticsName -g $resourceGroupName --query "endpoint"
-$textAnalyticsEndPoint = az cognitiveservices account show --name $textAnalyticsName ---resource-group $resourceGroupName --query "endpoint"
+$textAnalyticsEndPoint = az cognitiveservices account show --name $textAnalyticsName --resource-group $resourceGroupName --query "endpoint"
 $speechToTextKey = az cognitiveservices account keys list --name $speechToTextName --resource-group $resourceGroupName --query "key1"
 
 Write-Output "Textnalytics key is: $textAnalyticsKey, endpoint: $textAnalyticsEndPoint"
