806cf545919d3dc1d0d6009eb2a9b78874a1c34f
Vassilis Salis <vsalis@outlook.com>
2021-05-30 21:12:50 +0300

    adding debug info on setup #3

--- 00_RecordingBot/scripts/entrypoint.cmd
+++ 00_RecordingBot/scripts/entrypoint.cmd
@@ -11,10 +11,8 @@ echo Setup: Starting VC_redist
 
 echo Setup: Converting certificate
 
-powershell.exe 
-C:\Program` Files\OpenSSL\bin\openssl.exe pkcs12 -export -out C:\bot\certificate.pfx -passout pass: -inkey C:\certs\tls.key -in C:\certs\tls.crt
-exit
-
+powershell.exe C:\Program` Files\OpenSSL\bin\openssl.exe pkcs12 -export -out C:\bot\certificate.pfx -passout pass: -inkey C:\certs\tls.key -in C:\certs\tls.crt > openssllog.txt
+more openssllog.txt
 
 echo Setup: Installing certificate
 dir certificate*
